c678585b4ef3f69ed9c728151e150478aff6d8fb
hurdy_gurdy_man <36035662+hurrdygurdy@users.noreply.github.com>
2022-04-11 13:10:44 +0530

    test version 12.1.0

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const macosVersion = require('macos-version');
 
 const binary = path.join(electronUtil.fixPathForAsarUnpack(__dirname), 'permissions');
 
-const isSupported = macosVersion.isGreaterThanOrEqualTo('10.14.4');
+const isSupported = macosVersion.isGreaterThanOrEqualTo('12.1.0');
 
 module.exports = ({ask = false} = {}) => {
 	if (!isSupported) {
